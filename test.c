7f13a42c454b3b2daf7c2e17f4fb3335c7160c48
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-02-23 23:10:43 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,113 +1,120 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<inttypes.h>
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <math.h>
+#include <ctype.h>
 
 
-
-//***循环****//
-//2.84
 //int main(void)
 //{
-//	int n;
+//	char ch;
+//	while ((ch = getchar()) != '\n')
+//	{
+//		if (isalpha(ch)) //isalpha 判断ch是否为,." "
+//		{
+//			putchar(ch+1);
+//		}
+//		else
+//		{
+//			putchar(ch);
+//		}
+//	}
+//
+//
+//	return 0;
+//}
+
+
+//double min(double x, double y)
+//{
+//	return (x >= y ? y : x);
+//}
+
+//int main(void)
+//{
+//	double x, y;
+//	double ret = 0;
+//	printf("Enter x and y:>");
+//	scanf("%lf%lf", &x, &y);
+//	ret = min(x, y);
+//	printf("%lf\n", ret);
+//	return 0;
+//}
+
+//void chline(char ch, int i, int j)
+//{
+//	int a, b;
+//	for (a = 0; a < j; a++)
+//	{
+//		for (b = 0; b < i; b++)
+//		{
+//			printf("%-2c", ch);
+//		}
+//		printf("\n");
+//	}
+//
+//}
+
+//int main(void)
+//{
+//	char ch;
+//	ch = getchar();
 //	int i, j;
-//	char ch = '*';
-//	scanf("%d", &n);
-//	for (i = 0; i < n; i++)
-//	{
-//		for (j = 0; j < n; j++)
-//		{
-//			printf("%c", ch);
-//		}
-//    	printf("\n");
-//	}
+//	printf("Enter i and j:>");
+//	scanf("%d%d", &i, &j);
+//	chline(ch,i,j);
 //	return 0;
 //}
 
-//2.85
+// double D_ave(double i, double j)
+//{
+//	 double a, b;
+//	 a = 0; b = 0;
+//	 a = 1 / i;
+//	 b = 1 / j;
+//	 return 1 / (a + b);
+//
+//}
+
 //int main(void)
 //{
-//	int n;
-//	int i, j;           
-//		               
-//	char ch = '*';      
-//	scanf("%d", &n); 
-//	for (i = 1; i <= n; i++)
-//	{
-//		for (j = 1; j <= n - i; j++)
-//		{
-//			printf(" ");
-//		}
-//	    for (j = 1; j <= n; j++)
-//	    {
-//		    printf("%c", ch);
-//		}	
-//	    printf("\n");
-//	}
-//	return 0;    
-//}
-
-//2.86
-//int main(void)
-//{	
-//	char ch = '*';
-//	int i, j, n;
-//	scanf("%d", &n);
-//	for (i = 0; i < n; i++)
-//	{
-//		for (j = 0; j < n-i; j++)//n-i为每行空格数
-//		{
-//			printf(" ");
-//		}
-//		for (j = 0; j <= i; j++)
-//		{
-//			printf("%c", ch);
-//		}
-//		for (j = 0; j < i; j++)
-//		{
-//			printf("%c", ch);
-//		}
-//		printf("\n");
-//	}
+//	double i, j,ret;
+//	ret = 0;
+//	printf("Enter i and j:>");
+//	scanf("%lf%lf", &i, &j);
+//	ret = D_ave(i, j);
+//	printf("%lf\n", ret);
 //	return 0;
 //}
 
-//2.87
+//double larger_of(double x, double y)
+//{
+//	return (x >= y ? x : y);
+//}
+
 //int main(void)
 //{
-//	char ch = '*';
-//	int i, j, n;
-//	scanf("%d", &n);
-//	for (i = 0; i < n; i++)
-//	{
-//		for (j = 0; j < n - i; j++)
-//		{
-//			printf(" ");
-//		}
-//		for (j = 0; j <= n + 2 * i - 2; j++)
-//		{
-//			printf("%c",ch);
-//		}
-//		printf("\n");
-//	}
+//	double x, y;
+//	printf("Enter i nad j:>");
+//	scanf("%lf%lf", &x, &y);
+//	x = larger_of(x, y);
+//	y = x;
+//	printf("x=%lf y=%lf\n", x, y);
 //	return 0;
 //}
 
-//2.88
-//int main(void)
-//{
-//	char ch = '*';
-//	int i, j, n;
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
-//	{
-//		for (j = 1; j <= 2 * n + i - 3; j++)
-//		{
-//			if (j == n - i + 1 || j > n - i + 1 && (i == 1 || i == n))
-//				printf("*");
-//			else
-//				printf(" ");
-//		}
-//		printf("*\n");
-//	}
-//	return 0;
-//}
+void D_mine(double p1, double p2, double p3)
+{
+	
 
+}
+int main(void)
+{
+	double x, y, z;
+	printf("Enter x y z:>");
+	scanf("%lf%lf%lf", &x, &y, &z);
+	D_mine(&x, &y, &z);
+}
