23ddfe2fbc4dde24ee81c440f4ca37459fe81d1f
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-02-25 19:35:36 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,329 +1,345 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 
 
-#include<inttypes.h>
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
+#include <stdio.h>
 #include <math.h>
-#include <ctype.h>
 
 
-//int main(void)
+//void Q_jfc(a, b, k)
 //{
-//	char ch;
-//	while ((ch = getchar()) != '\n')
-//	{
-//		if (isalpha(ch)) //isalpha 判断ch是否为,." "
-//		{
-//			putchar(ch+1);
-//		}
-//		else
-//		{
-//			putchar(ch);
-//		}
-//	}
+//	double x1, x2;
+//	x1 = (sqrt(k) - b) / 2 * a;
+//	x2 = (-sqrt(k) - b) / 2 * a;
+//	printf("%")
+//}
 //
 //
-//	return 0;
-//}
-
-
-//double min(double x, double y)
-//{
-//	return (x >= y ? y : x);
-//}
-
 //int main(void)
 //{
-//	double x, y;
-//	double ret = 0;
-//	printf("Enter x and y:>");
-//	scanf("%lf%lf", &x, &y);
-//	ret = min(x, y);
-//	printf("%lf\n", ret);
-//	return 0;
-//}
-
-//void chline(char ch, int i, int j)
-//{
-//	int a, b;
-//	for (a = 0; a < j; a++)
+//	double k;
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	k = b * b - (4 * a*c);
+//	if (k > 0)
 //	{
-//		for (b = 0; b < i; b++)
-//		{
-//			printf("%-2c", ch);
-//		}
-//		printf("\n");
+//		Q_jfc(a, b, k);
 //	}
+//	else if (k < 0)
+//	{
 //
-//}
-
-//int main(void)
-//{
-//	char ch;
-//	ch = getchar();
-//	int i, j;
-//	printf("Enter i and j:>");
-//	scanf("%d%d", &i, &j);
-//	chline(ch,i,j);
-//	return 0;
-//}
-
-// double D_ave(double i, double j)
-//{
-//	 double a, b;
-//	 a = 0; b = 0;
-//	 a = 1 / i;
-//	 b = 1 / j;
-//	 return 1 / (a + b);
-//
-//}
-
-//int main(void)
-//{
-//	double i, j,ret;
-//	ret = 0;
-//	printf("Enter i and j:>");
-//	scanf("%lf%lf", &i, &j);
-//	ret = D_ave(i, j);
-//	printf("%lf\n", ret);
-//	return 0;
-//}
-
-//double larger_of(double x, double y)
-//{
-//	return (x >= y ? x : y);
-//}
-
-//int main(void)
-//{
-//	double x, y;
-//	printf("Enter i nad j:>");
-//	scanf("%lf%lf", &x, &y);
-//	x = larger_of(x, y);
-//	y = x;
-//	printf("x=%lf y=%lf\n", x, y);
-//	return 0;
-//}
-
-//void Do_maxin(double*x, double*y, double*z)
-//{
-//	double t; double *p = &t;
-//	if (*x > *y)
-//	{
-//		*p = *x;
-//		*x = *y;
-//		*y = *p;
-//	}
-//	if (*x > *z)
-//	{
-//		*p = *x;
-//		*x = *z;
-//		*z = *p;
-//	}
-//	if (*y > *z)
-//	{
-//		*p = *y;
-//		*y = *z;
-//		*z = *p;
-//	}
-//}
-
-//int main(void)
-//{
-//	double i, j, k;
-//	scanf("%lf%lf%lf", &i, &j, &k);
-//	Do_maxin(&i, &j, &k);
-//	printf("%lf %lf %lf", i, j, k);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int n;
-//
-//	scanf("%d", &n);
-//	if (n <= 1e+9)
-//	{
-//		printf("%#o %d %#x", n, n, n);
 //	}
 //	else
 //	{
-//		;
-//	}
 //
-//	return 0;
-//}
-
-/*#define money 50
-#define time 0.4
-int main(void)
-{
-	int i;
-	double sum = 0;
-	scanf("%d", &i);
-	sum = money + i * time;
-	printf("%0.1lf", sum);
-	return 0;
-}*/
-
-//int main(void)
-//{
-//	int a, b;
-//	int sum = 0;
-//	double c;
-//	scanf("%d%lf%d", &a, &c, &b);
-//	a = a * (1e4);
-//	sum = (a / b) + (a - 0)*c;
-//	printf("%d", sum);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int arr[3];
-//	scanf("%d%d%d", &arr[0], &arr[1], &arr[2]);
-//	printf("%-8d%-8d%-8d", arr[0], arr[1], arr[2]);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	float a;
-//	double a;
-//	scanf("%lf", &a);
-//	printf("%0.12lf", a);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	char ch;
-//	int i;
-//	float j;
-//	double k;
-//	scanf("%c%d%f%lf", &ch, &i, &j, &k);
-//	printf("%c %4d %4.2f %4.12lf", ch, i, j, k);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	double a;
-//	scanf("%lf", &a);
-//	printf("%f\n", a);
-//	printf("%0.5f\n", a);
-//	printf("%e\n", a);
-//	printf("%g\n", a);
-//
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	char ch;
-//	scanf("%c", &ch);
-//	for (int i = 1; i <= 3; i++)
-//	{
-//		for (int j = 1; j <= 3-i; j++)
-//		{
-//			printf(" ");
-//		}
-//		for (int j = 1; j <= 2 * i - 1; j++)
-//		{
-//			printf("%c", ch);
-//		}
-//		printf("\n");
 //	}
 //	return 0;
 //}
 
+
+//#define N 10
 //int main(void)
 //{
+//	
 //	int n;
 //	int sum = 0;
-//	int a = 2;
 //	scanf("%d", &n);
-//	for (int i = 1; i <= n; i++)
+//	int arr[N][N];
+//	for (int i = 0; i < n; i++)
 //	{
-//		sum += a;
-//		a = 10 * a + 2;
-//	}
-//	printf("%d", sum);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int a, b;
-//	float c;
-//	float k = 0;
-//	int i, j; i = 0; j = 0;
-//	float sum = 0;
-//	scanf("%d%d%f", &a, &b, &c);
-//	for (int d = 1; d <= a; d++)
-//	{
-//		i += d;
-//	}
-//	for (int e = 1; e <= b; e++)
-//	{
-//		j += e * e;
-//	}
-//	for (int f = 1.0; f <= c; f++)
-//	{
-//		k += 1.0 / f;
-//	}
-//	sum = i + j + k;
-//	printf("%0.2f", sum);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int N;
-//	double sum = 0;
-//	double t = 0.0;
-//	double m, n;
-//	m = 1.0; n = 2.0;
-//	scanf("%d", &N);
-//	for (int i = 1; i <= N; i++)
-//	{
-//		sum += n / m;
-//		t = m;
-//		m = n;
-//		n = m + t;
-//	}
-//	printf("%0.2lf", sum);
-//	return 0;
-//}
-
-//int main(void)
-//{
-//	int N;
-//	int s = 1;
-//	scanf("%d", &N);
-//	for (int i = 1; i < N; i++)
-//	{
-//		s = (s + 1) * 2;
-//	}
-//	printf("%d", s);
-//	return 0;
-//}
-
-
-//int main()
-//{
-//	int N, j, i;
-//	scanf("%d", &N);
-//	for (i = 2; i <= n; i++)
-//	{
-//		int flag = 1;
-//		for (j = 2; j < i; j++)
+//		for (int j = 0; j < n; j++)
 //		{
-//			if (i%j == 0)
-//				flag = 0;
+//			scanf("%d", &arr[i][j]);
 //		}
-//		if (flag == 1)
-//			printf("%d\n", i);
 //	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (i == j||i+j==n-1)
+//				sum += arr[i][j];
+//		}
+//	}
+//
+//		printf("%d", sum);
+//
+//	return 0;
+//}
+
+
+
+//int main(void)
+//{
+//	int arr[3][3];
+//	for (int i = 0; i < 3; i++)
+//	{
+//		scanf("%d", &arr[0][i]);
+//	}
+//	for (int i = 0; i < 3; i++)
+//	{
+//		scanf("%d", &arr[1][i]);
+//	}
+//	for (int i = 0; i < 3; i++)
+//	{
+//		scanf("%d", &arr[2][i]);
+//	}
+//
+//	printf("%d ", arr[0][0] + arr[1][1] + arr[2][2]);
+//	printf("%d", arr[0][2] + arr[1][1] + arr[2][0]);
+//
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int arr[10];
+//	for (int i = 0; i <= 9; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (int j = 9; j >= 0; j--)
+//	{
+//		printf("%d ", arr[j]);
+//	}
+//
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int arr[100];
+//	int count = 0;
+//	int sum = 0;
+//	double s = 0;
+//	for (int i = 0; i < 20; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (int j = 0; j < 20; j++)
+//	{
+//		if (arr[j] < 0)
+//		{
+//			count++;
+//		}
+//		if(arr[j]>0)
+//		{
+//			sum += arr[j];
+//		}
+//	}
+//	s = (sum) / (20.0 - count);
+//	printf("%d\n", count);
+//	printf("%0.2lf\n", s);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	for (int i = 10; i <= 1000; i++)
+//	{
+//		if (i % 2 == 0 && i % 3 == 0 && i % 7 == 0)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//			printf("%d*%d=%-3d", j, i, i*j);
+//		puts("");
+//	}
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int a;
+//	int i, j, k, l;
+//	i = 0; j = 0; k = 0; l = 0;
+//	scanf("%d", &a);
+//	i = a % 10;  //个位
+//	j = a / 10;
+//	k = j % 10;  //十位
+//	l = a / 100; //百位
+//	printf("%d %d %d", i, k, l);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	double M, m, g;
+//	int N;
+//	double h = 0;
+//	double s = 0;
+//	scanf("%lf%d", &M, &N);
+//	m = M;
+//	g = M;
+//	for (int i = 0; i < N; i++)
+//	{
+//		M /= 2;
+//	}
+//	for (int j =0; j < N; j++)
+//	{
+//		h += m;
+//		m = m / 2;
+//	}
+//	s = 2 * h - g;
+//	printf("%0.2lf %0.2lf",M, s);
+//	
+//	return 0;
+//}
+
+
+//int GCD(int x, int y)
+//{
+//	if (y == 0)
+//		return x;
+//	else
+//		return GCD(y, x%y);
+//}
+//
+//int LCD(int ret1, int m, int n)
+//{
+//	return (m / ret1)*n;
+//}
+//
+//int main(void)
+//{
+//	int m, n;
+//	int ret1 = 0;
+//	int ret2 = 0;
+//	scanf("%d%d", &m, &n);
+//	ret1 = GCD(m, n);
+//	ret2 = LCD(ret1, m, n);
+//	printf("%d %d", ret1, ret2);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int N, Sum;
+//	int a = 2;
+//	Sum = 0;
+//	scanf("%d", &N);
+//	for (int i = 0; i < N; i++)
+//	{
+//		Sum += a;
+//		a += 3;
+//	}
+//	printf("%d\n", Sum);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int a[100];
+//	int n;
+//	int S = 0;
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	for (int j = 0; j < n; j++)
+//	{
+//		if (a[j] % 2 == 0)
+//		{
+//			S += a[j];
+//		}
+//	}
+//	printf("%d", S);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	double F;
+//	double C = 0;
+//	scanf("%lf", &F);
+//	C = ((F - 32) * 5) / 9;
+//	printf("%0.2lf", C);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int i, j, k, l;
+//	i = 0; j = 0; k = 0; l = 0;
+//	for (int a = 100; a <= 999; a++)
+//	{
+//			i = a % 10;
+//			j = a / 10;
+//			k = j % 10;
+//			l = a / 100;
+//			if (i*i*i + k * k*k + l * l*l == a)
+//			{
+//				printf("%d\n", a);
+//			}
+//
+//	}
+//	
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int a;
+//	int i, j, k, l;
+//	i = 0; j = 0; k = 0; l = 0;
+//	scanf("%d", &a);
+//	if (a <= 999 && a >= 100)
+//	{
+//		i = a % 10;
+//		j = a / 10;
+//		k = j % 10;
+//		l = a / 100;
+//	}
+//	if (i*i*i + k * k*k + l * l*l == a)
+//	{
+//		printf("1");
+//	}
+//	else
+//	{
+//		printf("0");
+//	}
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int arr[10];
+//	int Count = 0;
+//	int S = 0;
+//	float Ave = 0;
+//	scanf("%d%d%d%d%d%d%d%d%d%d",
+//		&arr[0], &arr[1], &arr[2], &arr[3], &arr[4],
+//		&arr[5], &arr[6] ,& arr[7], &arr[8], &arr[9]);
+//	for (int i = 0; i < 10; i++)
+//	{
+//		S += arr[i];
+//	}
+//	Ave = S / 10.0;
+//	for (int j = 0; j < 10; j++)
+//	{
+//		if (arr[j ]> Ave)
+//		{
+//			Count++;
+//		}
+//	}
+//	printf("%d\n", Count);
+//	return 0;
 //}
