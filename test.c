0c5702a754757dff50a4d53a09f18d88972f41e1
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-02-27 23:22:15 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,137 +1,204 @@
 #define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 
-
-int main(void)
+//输入10个整数，将其中最小的数与第一个数对换，把最大的数与最后一个数对换
+//写三个函数: ①输入10个数；②进行处理；③输出10个数
+	
+#define N 10
+void Swap(int arr[N], int x, int y)
 {
-	return 0;
+	int temp = 0;
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
 }
 
+void Print(int arr[N], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+void Scan_f(int arr[N],int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+}
+
+void Sort_plus(int arr[N])
+{
+	int minpos = 0;
+	int maxpos = N - 1;
+	for (int j = 0; j < N; j++)
+	{
+		if (arr[minpos] > arr[j])
+		{
+			minpos = j;
+		}
+		Swap(arr, 0, minpos);
+	}
+	for (int j = 0; j < N; j++)
+	{
+		if (arr[maxpos] < arr[j])
+		{
+			maxpos = j;
+		}
+		Swap(arr, N - 1, maxpos);
+	}
+
+}
+int main(void)
+{
+	int arr[N];
+	Scan_f(arr,N);
+	Sort_plus(arr);
+	Print(arr, N);
+	return 0;
+}
+	
 
 
-//void K_fen(int a)
+
+
+//编写一个程序，输入a、b、c三个值，输出其中最大值
+//int Max_find(int max, int a, int b, int c)
 //{
-//	int i, j, k, l, m, n;
-//	i = 0; j = 0; k = 0;
-//	l = 0; m = 0; n = 0;
-//	i = a % 10;
-//	j = a / 10;
-//	k = j % 10;
-//	l = j / 10;
-//	m = l % 10;
-//	n = a / 1000;
-//	printf("%d %d %d %d", n, m, k, i);
+//	if (max < a)
+//	{
+//		max = a;
+//	}
+//	if (max < b)
+//	{
+//		max = b;
+//	}
+//	if (max < c)
+//	{
+//		max = c;
+//	}
+//	printf("%d", max);
 //}
 //
 //int main(void)
 //{
-//	int a;
-//	scanf("%d", &a);
-//	K_fen(a);
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int max = 0;
+//    Max_find(max, a, b, c);
 //	return 0;
 //}
 
 
-
-//int yuanyin(char a[], char b[])
-//{
-//	int i, j = 0, k, l;
-//	char x;
-//	l = strlen(a);
-//	for (i = 0; i < l; i++)
-//	{
-//		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u')
-//		{
-//			b[j] = a[i];
-//			j++;
-//		}
-//	}
-//	b[j] = '\0';
-//	k = strlen(b);
-//	for (i = 0; i < k; i++)
-//	{
-//		for (j = i; j < k; j++)
-//		{
-//			if (b[i] > b[j])
-//			{
-//				x = b[j]; b[j] = b[i]; b[i] = x;
-//			}
-//		}
-//	}
-//	return 0;
-//}
+//计算t=1+1/2+1/3+...+1/n
 //int main(void)
 //{
-//	char a[100], b[100];
-//	gets(a);
-//	yuanyin(a, b);
-//	puts(b);
+//	double t = 0;
+//	int n;
+//	scanf("%d", &n);
+//	for (double i = 1.0; i <= n; i++)
+//	{
+//		t += 1.0 / i;
+//	}
+//	printf("%0.6lf", t);
 //	return 0;
 //}
 
-//#define N 3
+
+//计算1~N之间所有奇数之和
+
 //int main(void)
-////{
-//	int arr[N][N];
-//	int tem = 0;
-//	for (int i = 0; i < N; i++)
+//{
+//	int N;
+//	int S = 0;
+//	scanf("%d", &N);
+//	for (int i = 1; i <= N; i++)
 //	{
-//		for (int j = 0; j < N; j++)
+//		if (i % 2 != 0)
 //		{
-//			scanf("%d", &arr[i][j]);
+//			S += i;
 //		}
 //	}
-//	for (int i = 0; i < N; i++)
-//	{
-//		for (int j = 0; j < N; j++)
-//		{
-//			if ((i + j == 1 || i + j == 2 || i + j == 3) && i != j)
-//			{
-//				tem = arr[i][j];
-//				arr[i][j] = arr[j][i];
-//				arr[j][i] = tem;
-//			}
-//		}
-//	}
-//	for (int i = 0; i < N; i++)
-//	{
-//		for (int j = 0; j < N; j++)
-//		{
-//			printf("%d ", arr[j][i]);
-//		}
-//		printf("\n");
-//	}
-//
+//	printf("%d", S);
 //	return 0;
 //}
 
 
-//void Is_prime(int n)
+
+//求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个一位的整数
+
+//int main(void)
 //{
-//	int a = 0;
-//	for (int i = 2; i < n; i++)
+//	int S = 0;
+//	int a, n;
+//	int m;
+//	scanf("%d%d", &a, &n);
+//	m = a;
+//	if (n >= 1 && a <= 9)
 //	{
-//		if (n%i == 0)
+//		for (int i = 1; i <= n; i++)
 //		{
-//			a++;
+//			S += a;
+//			a = a * 10 + m;
 //		}
 //	}
-//	if (a == 0)
-//	{
-//		printf("prime");
-//	}
-//	else
-//	{
-//		printf("not prime");
-//	}
+//	printf("%d", S); 
+//	return 0;
 //}
-//
+
+
+
+//最大数问题
+
 //int main(void)
 //{
 //	int n;
+//	int j;
 //	scanf("%d", &n);
-//	Is_prime(n);
+//	j = n;
+//	while (n != -1)
+//	{
+//		if (n >= j)
+//			j = n;
+//		scanf("%d", &n);
+//	}
+//	printf("%d\n", j);
+//
+//	return 0;
+//}
+
+
+
+
+//斐波那契数列
+
+//int  F(int N)
+//{
+//	if (N <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return F(N - 1) + F(N - 2);
+//	}
+//}
+//int main(void)
+//{
+//	int N;
+//	int ret = 0;
+//	scanf("%d", &N);
+//	if (N <= 40)
+//	{
+//		for (int i = 1; i <= N; i++)
+//		{
+//			ret = F(i);
+//			printf("%d ", ret);
+//		}
+//	}
 //	return 0;
 //}
