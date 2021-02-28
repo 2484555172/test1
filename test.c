3ef5a260dde0c0df25153268fdba1ed019848dd2
2484555172 <79183048+2484555172@users.noreply.github.com>
2021-02-28 23:38:48 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -3,202 +3,161 @@
 #include <stdio.h>
 #include <math.h>
 
-//输入10个整数，将其中最小的数与第一个数对换，把最大的数与最后一个数对换
-//写三个函数: ①输入10个数；②进行处理；③输出10个数
-	
-#define N 10
-void Swap(int arr[N], int x, int y)
-{
-	int temp = 0;
-	temp = arr[x];
-	arr[x] = arr[y];
-	arr[y] = temp;
-}
-
-void Print(int arr[N], int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-}
-
-void Scan_f(int arr[N],int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-}
-
-void Sort_plus(int arr[N])
-{
-	int minpos = 0;
-	int maxpos = N - 1;
-	for (int j = 0; j < N; j++)
-	{
-		if (arr[minpos] > arr[j])
-		{
-			minpos = j;
-		}
-		Swap(arr, 0, minpos);
-	}
-	for (int j = 0; j < N; j++)
-	{
-		if (arr[maxpos] < arr[j])
-		{
-			maxpos = j;
-		}
-		Swap(arr, N - 1, maxpos);
-	}
-
-}
-int main(void)
-{
-	int arr[N];
-	Scan_f(arr,N);
-	Sort_plus(arr);
-	Print(arr, N);
-	return 0;
-}
-	
 
 
 
-
-//编写一个程序，输入a、b、c三个值，输出其中最大值
-//int Max_find(int max, int a, int b, int c)
-//{
-//	if (max < a)
-//	{
-//		max = a;
-//	}
-//	if (max < b)
-//	{
-//		max = b;
-//	}
-//	if (max < c)
-//	{
-//		max = c;
-//	}
-//	printf("%d", max);
-//}
+//求1+2!+3!+...+N!的和
 //
-//int main(void)
+//long long Factorial(int j)
 //{
-//	int a, b, c;
-//	scanf("%d%d%d", &a, &b, &c);
-//	int max = 0;
-//    Max_find(max, a, b, c);
-//	return 0;
-//}
-
-
-//计算t=1+1/2+1/3+...+1/n
-//int main(void)
-//{
-//	double t = 0;
-//	int n;
-//	scanf("%d", &n);
-//	for (double i = 1.0; i <= n; i++)
-//	{
-//		t += 1.0 / i;
-//	}
-//	printf("%0.6lf", t);
-//	return 0;
-//}
-
-
-//计算1~N之间所有奇数之和
-
-//int main(void)
-//{
-//	int N;
-//	int S = 0;
-//	scanf("%d", &N);
-//	for (int i = 1; i <= N; i++)
-//	{
-//		if (i % 2 != 0)
-//		{
-//			S += i;
-//		}
-//	}
-//	printf("%d", S);
-//	return 0;
-//}
-
-
-
-//求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个一位的整数
-
-//int main(void)
-//{
-//	int S = 0;
-//	int a, n;
-//	int m;
-//	scanf("%d%d", &a, &n);
-//	m = a;
-//	if (n >= 1 && a <= 9)
-//	{
-//		for (int i = 1; i <= n; i++)
-//		{
-//			S += a;
-//			a = a * 10 + m;
-//		}
-//	}
-//	printf("%d", S); 
-//	return 0;
-//}
-
-
-
-//最大数问题
-
-//int main(void)
-//{
-//	int n;
-//	int j;
-//	scanf("%d", &n);
-//	j = n;
-//	while (n != -1)
-//	{
-//		if (n >= j)
-//			j = n;
-//		scanf("%d", &n);
-//	}
-//	printf("%d\n", j);
-//
-//	return 0;
-//}
-
-
-
-
-//斐波那契数列
-
-//int  F(int N)
-//{
-//	if (N <= 2)
+//	if (j == 1)
 //	{
 //		return 1;
 //	}
 //	else
 //	{
-//		return F(N - 1) + F(N - 2);
+//		return j * Factorial(j - 1);
 //	}
 //}
+//
 //int main(void)
 //{
-//	int N;
-//	int ret = 0;
-//	scanf("%d", &N);
-//	if (N <= 40)
+//	int n;
+//	long long S = 0;
+//	scanf("%d", &n);
+//	if (n <= 20&&n>0)
 //	{
-//		for (int i = 1; i <= N; i++)
+//		for (int i = 1; i <= n; i++)
 //		{
-//			ret = F(i);
-//			printf("%d ", ret);
+//			S += Factorial(i);
+//		}
+//	}
+//	printf("%lld", S);
+//	return 0;
+//}
+
+
+
+
+/*角谷猜想:日本一位中学生发现一个奇妙的“定理”,请角谷教授证明,
+而教授无能为力，于是产生角谷猜想猜想的内容是：任给一个自然数，若为偶数除以2,
+若为奇数则乘3加1,得到一个新的自然数后按照上面的法则继续演算,
+若干次后得到的结果必然为1。请编程验证:*/
+
+//int main(void)
+//{
+//
+//	int n;
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//	scanf("%d", &n);
+//	while (n != 1)
+//	{
+//		if (n % 2 == 0)
+//		{
+//			printf("%d/%d=%d\n", n, b, n / b);
+//			n = n / 2;
+//		}
+//		else
+//		{
+//			printf("%d*%d+%d=%d\n", n, c, a, n*c + a);
+//			n = 3 * n + 1;
 //		}
 //	}
 //	return 0;
 //}
+
+
+// 验证尼科彻斯定理,即：任何一个整数m的立方都可以写成m个连续奇数之和
+
+//#define M 100
+//void Print(int arr[M],int j,int m)
+//{
+//
+//	printf("%d", arr[j]);
+//	if (j < m - 1)
+//	{
+//		printf("+");
+//	}
+//}
+//int main(void)
+//{
+//	int m;
+//	int S = 0;
+//	scanf("%d", &m);
+//	int a = m * (m - 1) + 1;
+//	int arr[M];
+//	arr[0] = a;
+//	for (int i =0;i<m;i++)
+//	{
+//		S += a;
+//		 a+= 2;
+//		 arr[i+1] = a;
+//	}
+//	printf("%d*%d*%d=%d=", m, m, m, S);
+//	for (int j = 0; j < m; j++)
+//	{
+//		Print(arr,j,m);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//按递增顺序依次列出所有分母为40，分子小于40的最简分数
+
+//int main(void)
+//{
+//	for (int i = 1; i < 40; i++)
+//	{
+//		if (i % 2 != 0 && i % 5 != 0)
+//		{
+//			printf("%d/40,", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
+
+/*一个自然数被8除余1,所得的商被8除也余1,再将第二次的商被8除后余7, 最后得到一个商为a
+又知这个自然数被17除余4.所得的商被17除余15,最后得到一个商是a的2倍求这个自然数*/
+
+//int main(void)
+//{
+//	int n = 0;
+//	for (int a = 1; a < 10; a++)
+//	{
+//		if ((64 * a + 57) * 8 + 1 == (34 * a + 15) * 17 + 4)
+//		{
+//			n = (64 * a + 57) * 8 + 1;
+//			printf("%d", n);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	for (int i = 1; i < 9; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-3d", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
